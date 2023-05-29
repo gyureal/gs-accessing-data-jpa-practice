f20c956d954ff7979e9a44fb914c472a1295979f
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-05-29 21:23:29 +0900

    Initial commit

---
+++ src/main/java/com/example/gsaccessingdatajpapractice/GsAccessingDataJpaPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.gsaccessingdatajpapractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsAccessingDataJpaPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GsAccessingDataJpaPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gsaccessingdatajpapractice/GsAccessingDataJpaPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gsaccessingdatajpapractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsAccessingDataJpaPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
